e5fe9d49400a61415c5a76ddf8eb44315bbea503
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-08-08 22:06:19 -0400

    final change of "about" section

--- routes/gameSessions.js
+++ routes/gameSessions.js
@@ -17,13 +17,6 @@ let findGameSession = async (req, res, next) => {
   next();
 };
 
-// get info about the REST Api
-router.use("/about", (req, res) => {
-  res.send(`<h1><a>tjcharm.github.io</a></h1>
-        <h2>A node.js REST Api hosted on heroku, created by Traceton Timmerman for my js games.</h2>
-        <h2>use -> <a>https://gamescoreserver.herokuapp.com/gameSessions</a></h2>`);
-});
-
 // get all game sessions.
 router.get("/", async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -19,6 +19,12 @@ database.once("open", () => {
 
 app.use(express.json());
 
+app.use("/about", (req, res) => {
+  res.send(`<h1><a>tjcharm.github.io</a></h1>
+          <h2>A node.js REST Api hosted on heroku, created by Traceton Timmerman for my js games.</h2>
+          <h2>use -> <a>https://gamescoreserver.herokuapp.com/gameSessions</a></h2>`);
+});
+
 app.use("/gameSessions", gameSessionsRouter);
 
 app.listen(process.env.PORT, () => {
